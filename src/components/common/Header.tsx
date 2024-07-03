5e589566a8d821a2138f2372ff6d87ef75348750
R3D1NM <r3dinm@gmail.com>
2024-07-03 15:11:54 +0900

    [feat] apply a tag to header logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,9 @@
 const Header = () => {
   return (
     <header className="bg-black flex justify-center min-h-[5svh] align-middle">
+      <a href="/">
         <h2 className='text-white text-3xl font-Bayon'> One!t </h2>
+      </a>
     </header>
   )
 }
