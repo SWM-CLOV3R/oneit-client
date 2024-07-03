97eb6102c077fb6a672df6bdf5e92e689ac060b2
R3D1NM <r3dinm@gmail.com>
2024-07-03 17:00:16 +0900

    [feat] handle browser back button

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,56 +1,67 @@
-import { useAtom, useAtomValue, useSetAtom } from 'jotai'
-import { depth, loading, question, recipient } from '@/config/atoms'
+import { useAtomValue, useSetAtom } from 'jotai'
+import { loading, recipient } from '@/config/atoms'
 import { useNavigate, useParams } from 'react-router-dom';
 import { Spinner } from '@/components/ui/spinner';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogHeader } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { finishChat, next } from '@/api/chat';
+import question from "@/data/question(2).json";
+import { Question } from "@/config/types";
 
 // MAX DEPTH of the chat
 const MAXDEPTH = 4
 
+type AnswerOptions = {
+    [key: string]: number[];
+};
+
+type Questions = {
+    [question: string]: AnswerOptions;
+};
+const questionData: Questions = question;
+const questionList: Question[] = Object.keys(questionData).map((question: string, idx: number) => {
+    return {
+        question,
+        options: Object.keys(questionData[question])
+    };
+})
+
 const Quiz = () => {
-    const questions = useAtomValue(question)
+
     const isloading = useAtomValue(loading)
-    const [currentQuestion,setCurrentQuestion] = useAtom(depth)
     const getNextQuestion = useSetAtom(next)
     const endChat = useSetAtom(finishChat)
     const userRecipient = useAtomValue(recipient)
-
+    
     const navigate = useNavigate();
-    const { chatID } = useParams()
+    const params = useParams()
+    const chatID = params.chatID
+    const currentDepth = Number(params.currentDepth)
     const [error, setError] = useState(false)
     const [selected, setSelected] = useState(0)
 
     const heads = ["",`${userRecipient}의 `,`${userRecipient}이(가) `, `${userRecipient}의 `, `${userRecipient}와(과) `]
 
     // Debugging logs
-    console.log('Questions:', questions, 'Loading:', isloading);
+    // console.log('Questions:', questionList[currentDepth], 'Loading:', isloading);
 
-    useEffect(() => {
-        //handle browser back button
-        window.onpopstate = () => {
-            setCurrentQuestion(1)
-        }
-    }, [])
     
     
-
     const handleAnswerClick = async (index:number) => {
         setSelected(index)
-        const depth = currentQuestion
-        console.log('Depth:', depth, 'Selected:', index);
+        console.log('Depth:', currentDepth, 'Selected:', index);
         
-        if (depth < MAXDEPTH) {
+        if (currentDepth < MAXDEPTH) {
             try {
-                await getNextQuestion({question: questions.question, answer: questions.options[index]})
+                await getNextQuestion({question: questionList[currentDepth].question, answer: questionList[currentDepth].options[index]}, currentDepth)
+                navigate(`/quiz/${chatID}/${currentDepth+1}`);
             } catch (error) {
                 setError(true)
             }
-        }else if (depth === MAXDEPTH ) {
+        }else if (currentDepth === MAXDEPTH ) {
             try {
-                await endChat(chatID, {question: questions.question, answer: questions.options[index]})
+                await endChat(chatID, {question: questionList[currentDepth].question, answer: questionList[currentDepth].options[index]})
                 navigate(`/result/${chatID}`);
             } catch (error) {
                 setError(true)
@@ -69,10 +80,10 @@ const Quiz = () => {
         <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md p-8 max-w-md w-full">
             
             <h2 className="text-2xl font-bold mb-4 text-gray-900 dark:text-gray-100">
-                {heads[currentQuestion]+questions.question}
+                {heads[currentDepth]+questionList[currentDepth].question}
             </h2>
             <div className="grid grid-cols-1 gap-4">
-                {questions.options.map((option, index) => (
+                {questionList[currentDepth].options.map((option, index) => (
                 <Button
                     key={index}
                     onClick={() => handleAnswerClick(index)}
@@ -91,10 +102,10 @@ const Quiz = () => {
                 </DialogHeader>
                 <DialogDescription>문제가 발생했습니다. 다시 시도해주세요.</DialogDescription>
                 <div className="flex justify-end gap-2">
-                    <Button variant="outline" onClick={() => {setError(false); setCurrentQuestion(1); navigate('/'); } }>
+                    <Button variant="outline" onClick={() => {setError(false);  navigate('/'); } }>
                     메인으로
                     </Button>
-                    <Button type="submit" onClick={() => {setError(false); setCurrentQuestion((prev)=>prev-1); handleAnswerClick(selected);} }>
+                    <Button type="submit" onClick={() => {setError(false); handleAnswerClick(selected);} }>
                     다시시도
                     </Button>
                 </div>
