395e798a15742f33215d101a84988d58773fc992
R3D1NM <r3dinm@gmail.com>
2024-07-09 16:50:20 +0900

    [feat] Share logic with sonner (ONE-92)

--- src/components/common/Share.tsx
+++ src/components/common/Share.tsx
@@ -1,5 +1,6 @@
 import { Share2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { Toaster, toast } from "sonner"
 
 interface ShareProps {
     title: string
@@ -11,20 +12,33 @@ const Share = (props: ShareProps) => {
     const {title, text, url} = props
 
     const onShare = async () => {
-        if(navigator.canShare(props)){
-            await navigator.share({
-                title,
-                text,
-                url
-            })
+        if(navigator.share&&navigator.canShare(props)){
+            try {
+                await navigator.share({
+                    title,
+                    text,
+                    url
+                })
+                toast('공유 성공')
+            } catch (error) {
+                toast('공유 실패')
+            }
+
         }else{
-            console.log('Web Share API not supported');
+            // console.log('Web Share API not supported');
+            try {
+                await navigator.clipboard.writeText(url)
+                toast("클립보드에 복사되었습니다.");
+            } catch (error) {
+                toast("공유 기능이 지원되지 않는 브라우저입니다.");
+            }
         }
     }
 
     return (
         <Button variant="ghost" onClick={onShare}>
             <Share2/>
+            <Toaster/>
         </Button>
     )
 }
