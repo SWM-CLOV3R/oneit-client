65bc71455cdc765dffab666d99d2f6f331c06f3a
R3D1NM <r3dinm@gmail.com>
2024-07-09 20:57:45 +0900

    [fix] case when hovering the image it covers the header (ONE-90)

--- src/components/Cards/ProductCard.tsx
+++ src/components/Cards/ProductCard.tsx
@@ -8,14 +8,14 @@ interface ProductCardProps {
 const ProductCard = (props: ProductCardProps) => {
     const {product} = props
     return (
-    <div className="bg-background rounded-lg overflow-hidden shadow-sm group">
+    <div className="rounded-lg overflow-hidden shadow-sm ">
         <Link to={`/product/${product.id}`} className="block">
             <img
                 src={product.image || "https://via.placeholder.com/400"}
                 alt={product.title}
                 width={400}
                 height={300}
-                className="w-full object-cover group-hover:opacity-80 transition-opacity"
+                className="relative z-[-10] w-full object-cover hover:opacity-80 transition-opacity"
             />
             <div className="p-4">
                 <h3 className="max-w-full  text-sm font-semibold mb-2 overflow-hidden whitespace-nowrap  overflow-ellipsis">{product.title}</h3>
