3400051312f304d57b7156a53a22a2f5f0ce668b
R3D1NM <r3dinm@gmail.com>
2024-07-09 18:51:08 +0900

    [fix] fix dependency error

--- src/api/chat.ts
+++ src/api/chat.ts
@@ -1,10 +1,10 @@
 import { update, ref ,set as write, serverTimestamp} from "firebase/database";
-import { db } from "@/config/firebase";
+import { db } from "@/lib/firebase";
 import { atom } from "jotai";
-import { gender, loading, occasion, priceRange, recipient, answers, gift, question as nextQuestion } from "@/config/atoms";
+import { gender, loading, occasion, priceRange, recipient, answers, gift, question as nextQuestion } from "@/lib/atoms";
 import product from "@/data/product(2).json";
 import question from "@/data/question(2).json";
-import { Answer, Product, Question } from "@/config/types";
+import { Answer, Product, Question } from "@/lib/types";
 
 type AnswerOptions = {
     [key: string]: number[];
@@ -51,85 +51,85 @@ export const next = atom(null, async (get,set, answer : Answer,currentDepth: num
     
 })
 
-const recommend = (gender: string, priceRange: number[], answers: Answer[]): Product[] => {
-    let pool: { [key: string]: Product } = { ...product };
+const recommend = (gender: string, priceRange: number[], answers: Answer[]) => {
+    // let pool: { [key: string]: Product } = { ...product };
 
-    // If the product pool has product with different gender, filter out the products
-    if (gender == "남성"){
-        questionData["성별"]?.["여성"].forEach((key: number) => {
-            delete pool[key.toString()];
-        })
-    } else {
-        questionData["성별"]?.["남성"].forEach((key: number) => {
-            delete pool[key.toString()];
-        })
-    }
-    console.log("Initial Pool:", pool);
+    // // If the product pool has product with different gender, filter out the products
+    // if (gender == "남성"){
+    //     questionData["성별"]?.["여성"].forEach((key: number) => {
+    //         delete pool[key.toString()];
+    //     })
+    // } else {
+    //     questionData["성별"]?.["남성"].forEach((key: number) => {
+    //         delete pool[key.toString()];
+    //     })
+    // }
+    // console.log("Initial Pool:", pool);
     
-    answers.forEach(answer => {
-        const matchingProductsKeys: number[] = questionData[answer.question]?.[answer.answer];
-        if (matchingProductsKeys && matchingProductsKeys.length > 0) {
-            const filteredPool: { [key: string]: Product } = {};
-            matchingProductsKeys.forEach((key: number) => {
-                if (pool[key.toString()]) {
-                    //If the price of the product is within the price range, add it to the filtered pool
-                    if (pool[key.toString()].price >= priceRange[0] && pool[key.toString()].price <= priceRange[1])
-                        filteredPool[key.toString()] = pool[key.toString()];
-                }
-            });
+    // answers.forEach(answer => {
+    //     const matchingProductsKeys: number[] = questionData[answer.question]?.[answer.answer];
+    //     if (matchingProductsKeys && matchingProductsKeys.length > 0) {
+    //         const filteredPool: { [key: string]: Product } = {};
+    //         matchingProductsKeys.forEach((key: number) => {
+    //             if (pool[key.toString()]) {
+    //                 //If the price of the product is within the price range, add it to the filtered pool
+    //                 if (pool[key.toString()].price >= priceRange[0] && pool[key.toString()].price <= priceRange[1])
+    //                     filteredPool[key.toString()] = pool[key.toString()];
+    //             }
+    //         });
 
-            // If filtering results in an empty pool but the original pool was not empty, do not apply the filter.
-            if (Object.keys(filteredPool).length > 0 || Object.keys(pool).length === 0) {
-                pool = filteredPool;
-            }
-        }
+    //         // If filtering results in an empty pool but the original pool was not empty, do not apply the filter.
+    //         if (Object.keys(filteredPool).length > 0 || Object.keys(pool).length === 0) {
+    //             pool = filteredPool;
+    //         }
+    //     }
 
-        console.log("Pool:", pool);
+    //     console.log("Pool:", pool);
         
-    });
+    // });
 
 
 
-    return Object.values(pool);
+    // return Object.values(pool);
 };
 
 
 export const finishChat = atom(null, async (get,set,chatID, answer:Answer) => {
-    set(answers, {...get(answers), answer})
-    set(loading, true)
-    const answerList = Object.values(get(answers))
-    console.log(answerList);
+    // set(answers, {...get(answers), answer})
+    // set(loading, true)
+    // const answerList = Object.values(get(answers))
+    // console.log(answerList);
     
-    try {
-        await update(
-            ref(db, `/chats/${chatID}`),
-            { 
-                answers: answerList,
-                modifiedAt: serverTimestamp()
-            }
-        );
+    // try {
+    //     await update(
+    //         ref(db, `/chats/${chatID}`),
+    //         { 
+    //             answers: answerList,
+    //             modifiedAt: serverTimestamp()
+    //         }
+    //     );
 
-        //get recommendation proudcts
-        const recommendList = recommend(get(gender),get(priceRange),answerList)
-        console.log(recommendList);
+    //     //get recommendation proudcts
+    //     const recommendList = recommend(get(gender),get(priceRange),answerList)
+    //     console.log(recommendList);
         
 
-        //select random product
-        const recommended = recommendList[Math.floor(Math.random() * recommendList.length)]
+    //     //select random product
+    //     const recommended = recommendList[Math.floor(Math.random() * recommendList.length)]
 
-        set(gift, recommended)
+    //     set(gift, recommended)
 
-        await update(
-            ref(db, `/chats/${chatID}`),
-            { result: recommended }
-        );
+    //     await update(
+    //         ref(db, `/chats/${chatID}`),
+    //         { result: recommended }
+    //     );
 
-    } catch (error) {
-        console.log(error);
-        throw new Error("Failed to finish chat");
-    } finally{
-        set(loading, false)
-    }
+    // } catch (error) {
+    //     console.log(error);
+    //     throw new Error("Failed to finish chat");
+    // } finally{
+    //     set(loading, false)
+    // }
 });
 
 // export const updateQuestion = atom(null, async (get,set,answer:Answer) => {
