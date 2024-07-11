c8876a41293efa7755474e5af0a3258ce25e08c5
R3D1NM <r3dinm@gmail.com>
2024-07-11 15:15:13 +0900

    [feat] add card to about page (ONE-279)

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
     return (
-    <div className='border-t-[0.5px] text-stone-300 text-xs justify-evenly flex flex-col items-center pr-2 min-h-[5svh] w-full relative z-[-100]'>
+    <div className='border-t-[0.5px] bg-stone-50 text-xs justify-evenly flex flex-col items-center pr-2 min-h-[4svh] w-full relative z-[-100]'>
         <span className="font-light font-Bayon">Team.CLOV3R</span>
     </div>
     )
