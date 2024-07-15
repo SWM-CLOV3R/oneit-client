2adf1162cee1a8aa8d25047b9e0517f3f4f68846
R3D1NM <r3dinm@gmail.com>
2024-07-15 15:53:12 +0900

    [feat] buttons on Result Page (ONE-335)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ const About = () => {
             </CardContent>
         </Card>
         <Card className="w-full bg-gradient-to-br mb-2 from-[#ffa0a0] to-[#ff9595] hover:from-[#FF9595] hover:to-[#FF9090] transition-colors duration-300 rounded-lg h-[20%] flex flex-col justify-between shadow-md border-0 max-w-md">
-            <Link to="/" className="w-full h-full flex flex-col justify-between">
+            <Link to="/recommend" className="w-full h-full flex flex-col justify-between">
             <CardHeader className='p-3'>
                 <CardTitle>선물 추천</CardTitle>
                 <CardDescription className='text-oneit-gray'>질문에 답하고 선물을 추천 받아 보세요!</CardDescription>
