523239488391b51f0c7a357bb390246355412d85
R3D1NM <r3dinm@gmail.com>
2024-08-02 01:00:53 +0900

    [feat] infinite scroll to load products (ONE-431)

--- src/pages/Curation.tsx
+++ src/pages/Curation.tsx
@@ -1,106 +1,74 @@
 import ProductCard from "@/components/Cards/ProductCard";
 import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { Product } from "@/lib/types";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import axios from '@/lib/axios';
 import { useQuery } from "@tanstack/react-query";
 import { Spinner } from "@/components/ui/spinner";
 import NotFound from "./NotFound";
+import { useProductListInfinite } from "@/api/product";
+
 
 const Curation = () => {
     const [currentPage, setCurrentPage] = useState(1)
     const [currentItems, setCurrentItems] = useState<Product[]>([]);
     const itemsPerPage = 6;
-
-    const fetchProductList = async (): Promise<Product[]> => {
-        return axios.get(`/v1/products`)
-            .then(res => {
-                if (res.status === 200 && res.data.isSuccess) {
-                    // console.log(res.data.result);
-                    return Promise.resolve(res.data.result)
-                } else {
-                    throw new Error(res.data.message)
-                }
-            })
-            .catch(err => {
-                console.error(err)
-                return Promise.reject(err)
-            })
-    }
-    const { data, isError, isLoading } = useQuery({ queryKey: ['productList'], queryFn: fetchProductList })
-
+    const { data, isLoading, isError, fetchNextPage, hasNextPage } = useProductListInfinite()
+    const nextFetchTargetRef = useRef<HTMLDivElement | null>(null); // ref 객체 생성
+    // console.log(fetchNextPage,hasNextPage);
     
+
     useEffect(() => {
-        const indexOfLastItem = currentPage * itemsPerPage;
-        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-        setCurrentItems(data?.slice(indexOfFirstItem, indexOfLastItem)||[] as Product[]);
-        // console.log(currentItems);
+        // console.log('useEffect');
         
-    }, [currentPage,data]);
-    
-    const totalPages = Math.ceil((data?.length||0) / itemsPerPage);
-    
-    const handlePageChange = (pageNumber : number) => {
-        setCurrentPage(pageNumber)
-        window.scrollTo(0, 0)
-    }
-    
-    const renderPaginationItems = () => {
-        const pages = [];
-        for (let i = 1; i <= totalPages; i++) {
-            if (i === 1 || i === totalPages || (i >= currentPage - 1 && i <= currentPage + 1)) {
-                pages.push(
-                    <PaginationItem key={i}>
-                        <PaginationLink href="#" isActive={i === currentPage} onClick={() => handlePageChange(i)}>
-                            {i}
-                        </PaginationLink>
-                    </PaginationItem>
-                );
-            } else if (i === currentPage - 2 || i === currentPage + 2) {
-                pages.push(
-                    <PaginationItem key={i}>
-                        <PaginationEllipsis />
-                    </PaginationItem>
-                );
-            }
-        }
-        return pages;
-    };
+        const options = {
+            root: null,
+            rootMargin: '30px',
+            threshold: 0.2, 
+        };
 
-    if(isLoading) return <Spinner/>
-    if(isError) return <NotFound/>
+        const fetchCallback: IntersectionObserverCallback = (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting && hasNextPage) {
+                    console.log('fetch next page');
+                    
+                    fetchNextPage?.();
+                    observer.unobserve(entry.target);
+                }
+            });
+        };
+        const observer = new IntersectionObserver(fetchCallback, options);
+        // console.log(nextFetchTargetRef.current);
+        
+        if (nextFetchTargetRef.current) {
+            // console.log('observe');
+            
+            observer.observe(nextFetchTargetRef.current);
+        }
+
+        return () => {
+            if (nextFetchTargetRef.current) {
+                observer.unobserve(nextFetchTargetRef.current);
+            }
+        };
+    }, [data, hasNextPage, fetchNextPage]);
+
+
+    if (isLoading) return <Spinner />
+    if (isError) return <NotFound />
 
     return (
         <div className="w-full mt-4 flex flex-col content-center justify-center align-middle items-center overflow-y-auto scrollbar-hide">
             <h2 className="text-xl font-bold"> Curation </h2>
             <div className="container py-5 px-2 grid grid-cols-2 gap-2">
-                {currentItems.map((product) => {
-                    return <ProductCard key={product.productIdx+currentPage} product={product}/>
-                })}
+                {data?.pages.map((page, pageIndex) => (
+                    page.map((product: Product, productIndex: number) => (
+                        <ProductCard key={`${pageIndex}-${productIndex}`} product={product} />
+                    ))
+                ))}
+                
             </div>
-            <Pagination className="max-w-full px-2">
-                <PaginationContent className="gap-0">
-                <PaginationItem>
-                    <PaginationPrevious
-                        href="#"
-                        className={
-                            currentPage <= 1 ? "pointer-events-none opacity-50" : undefined
-                        }
-                        onClick={() => handlePageChange(currentPage - 1)}
-                    />
-                </PaginationItem>
-                {renderPaginationItems()}
-                <PaginationItem>
-                    <PaginationNext
-                        href="#"
-                        onClick={() => handlePageChange(currentPage + 1)}
-                        className={
-                            currentPage >= totalPages ? "pointer-events-none opacity-50" : undefined
-                        }
-                    />
-                </PaginationItem>
-                </PaginationContent>
-            </Pagination>
+            {hasNextPage && <div ref={nextFetchTargetRef} className="col-span-2"></div>}
         </div>
     );
 };
