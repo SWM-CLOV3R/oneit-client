05b75067aad69b63e0c98053456e9a962ad0cadd
R3D1NM <r3dinm@gmail.com>
2024-08-02 22:47:17 +0900

    [feat] mypage button on Header (ONE-300)

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,87 +4,96 @@ import {atom} from 'jotai';
 import {atomWithDefault} from 'jotai/utils';
 
 const getMe = async () => {
-	//get user info
-	//todo: get user info from server
-	return axios
-		.get('/v1/kakao/user')
-		.then((res) => {
-			console.log(res);
+    //get user info
+    //todo: get user info from server
+    return axios
+        .get('/v1/kakao/user')
+        .then((res) => {
+            console.log(res);
 
-			if (res.status == 200 && res.data.isSuccess) {
-				return Promise.resolve(res.data.result);
-			} else {
-				throw new Error('Failed to get user info');
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-			return Promise.reject(err);
-		});
+            if (res.status == 200 && res.data.isSuccess) {
+                return Promise.resolve(res.data.result);
+            } else {
+                throw new Error('Failed to get user info');
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            return Promise.reject(err);
+        });
 };
 
 //Auth
 const getAuth = async (): Promise<User | null> => {
-	const token = {
-		access: localStorage.getItem('token'),
-		// refresh: cookies.get('refreshToken'),
-	};
-	console.log(token);
+    const token = {
+        access: localStorage.getItem('token'),
+        // refresh: cookies.get('refreshToken'),
+    };
+    console.log(token);
 
-	if (token.access) {
-		try {
-			const userInfo = await getMe();
-			return userInfo;
-		} catch (err) {
-			console.log(err);
-			return null;
-		}
-	}
+    if (token.access) {
+        try {
+            const userInfo = await getMe();
+            return userInfo;
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
 
-	// if (token.refresh) {
-	//     const accessToken = await getRefreshToken();
-	//     if (accessToken) {
-	//         localStorage.setItem('token', accessToken);
+    // if (token.refresh) {
+    //     const accessToken = await getRefreshToken();
+    //     if (accessToken) {
+    //         localStorage.setItem('token', accessToken);
 
-	//         const userInfo = await getMe();
-	//         if (userInfo) return userInfo;
-	//     }
-	// }
+    //         const userInfo = await getMe();
+    //         if (userInfo) return userInfo;
+    //     }
+    // }
 
-	//not logged in
-	return null;
+    //not logged in
+    return null;
 };
 
 export const authAtom = atomWithDefault(getAuth);
 authAtom.debugLabel = 'authAtom';
 
 export const updateAuthAtom = atom(null, async (get, set) => {
-	set(authAtom, getAuth());
+    set(authAtom, getAuth());
+});
+
+export const isLoginAtom = atom(async (get) => {
+    const user = await get(authAtom);
+    if (user) {
+        return true;
+    } else {
+        return false;
+    }
 });
 
 export const login = async (token: string) => {
-	try {
-		const res = await axios.post('/v1/kakao/login', {
-			accessToken: token,
-		});
+    try {
+        const res = await axios.post('/v1/kakao/login', {
+            accessToken: token,
+        });
 
-		if (res.status == 200 && res.data.isSuccess) {
-			// const { accessToken, refreshToken } = res.data as LoginResponse;
-			const accessToken = res.data.result.accessToken;
-			localStorage.setItem('token', accessToken);
-			// cookies.set("refreshToken", refreshToken, { path: "/", httpOnly: true });
-			return Promise.resolve();
-		} else {
-			throw new Error('Failed to login');
-		}
-	} catch (err) {
-		console.log(err);
-		return Promise.reject(err);
-	}
+        if (res.status == 200 && res.data.isSuccess) {
+            // const { accessToken, refreshToken } = res.data as LoginResponse;
+            const accessToken = res.data.result.accessToken;
+            localStorage.setItem('token', accessToken);
+            // cookies.set("refreshToken", refreshToken, { path: "/", httpOnly: true });
+            return Promise.resolve();
+        } else {
+            throw new Error('Failed to login');
+        }
+    } catch (err) {
+        console.log(err);
+        return Promise.reject(err);
+    }
 };
 
 export const logout = async () => {
-	localStorage.removeItem('token');
+    localStorage.removeItem('token');
 };
 
 // const getRefreshToken = async (): Promise<string> => {
