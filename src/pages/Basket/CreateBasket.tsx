a697bc9eb4c9275add8a1cbf0a8b06b56065c833
R3D1NM <r3dinm@gmail.com>
2024-08-03 15:26:27 +0900

    [feat] create basket card on basket page (ONE-302)

---
+++ src/pages/Basket/CreateBasket.tsx
@@ -0,0 +1,9 @@
+const CreateBasket = () => {
+    return (
+        <div className="flex flex-col content-center mt-3 w-full justify-center gap-2">
+            CreateBasket
+        </div>
+    );
+};
+
+export default CreateBasket;
