cbd8323bd79cac508cff1aa6c9178a7fc33213ee
R3D1NM <r3dinm@gmail.com>
2024-08-05 18:48:24 +0900

    [style] fix the postion of header, navbar, and scrollTop button

--- src/pages/Product/Curation.tsx
+++ src/pages/Product/Curation.tsx
@@ -8,80 +8,80 @@ import {Button} from '@/components/ui/button';
 import {ArrowUp} from 'lucide-react';
 
 const Curation = () => {
-	const {data, isLoading, isError, fetchNextPage, hasNextPage} =
-		useProductListInfinite();
-	const nextFetchTargetRef = useRef<HTMLDivElement | null>(null); // ref 객체 생성
-	// console.log(fetchNextPage,hasNextPage);
+    const {data, isLoading, isError, fetchNextPage, hasNextPage} =
+        useProductListInfinite();
+    const nextFetchTargetRef = useRef<HTMLDivElement | null>(null); // ref 객체 생성
+    // console.log(fetchNextPage,hasNextPage);
 
-	useEffect(() => {
-		// console.log('useEffect');
+    useEffect(() => {
+        // console.log('useEffect');
 
-		const options = {
-			root: null,
-			rootMargin: '30px',
-			threshold: 0.2,
-		};
+        const options = {
+            root: null,
+            rootMargin: '30px',
+            threshold: 0.2,
+        };
 
-		const fetchCallback: IntersectionObserverCallback = (
-			entries,
-			observer,
-		) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting && hasNextPage) {
-					console.log('fetch next page');
+        const fetchCallback: IntersectionObserverCallback = (
+            entries,
+            observer,
+        ) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting && hasNextPage) {
+                    console.log('fetch next page');
 
-					fetchNextPage?.();
-					observer.unobserve(entry.target);
-				}
-			});
-		};
-		const observer = new IntersectionObserver(fetchCallback, options);
-		// console.log(nextFetchTargetRef.current);
+                    fetchNextPage?.();
+                    observer.unobserve(entry.target);
+                }
+            });
+        };
+        const observer = new IntersectionObserver(fetchCallback, options);
+        // console.log(nextFetchTargetRef.current);
 
-		if (nextFetchTargetRef.current) {
-			// console.log('observe');
+        if (nextFetchTargetRef.current) {
+            // console.log('observe');
 
-			observer.observe(nextFetchTargetRef.current);
-		}
+            observer.observe(nextFetchTargetRef.current);
+        }
 
-		return () => {
-			if (nextFetchTargetRef.current) {
-				observer.unobserve(nextFetchTargetRef.current);
-			}
-		};
-	}, [data, hasNextPage, fetchNextPage]);
+        return () => {
+            if (nextFetchTargetRef.current) {
+                observer.unobserve(nextFetchTargetRef.current);
+            }
+        };
+    }, [data, hasNextPage, fetchNextPage]);
 
-	const scrollToTop = () => {
-		window.scrollTo({top: 0, behavior: 'smooth'});
-	};
+    const scrollToTop = () => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    };
 
-	if (isLoading) return <Spinner />;
-	if (isError) return <NotFound />;
+    if (isLoading) return <Spinner />;
+    if (isError) return <NotFound />;
 
-	return (
-		<div className="w-full mt-4 flex flex-col content-center justify-center align-middle items-center overflow-y-auto scrollbar-hide">
-			<h2 className="text-xl font-bold"> Curation </h2>
-			<div className="container py-5 px-2 grid grid-cols-2 gap-2">
-				{data?.pages.map((page, pageIndex) =>
-					page.map((product: Product, productIndex: number) => (
-						<ProductCard
-							key={`${pageIndex}-${productIndex}`}
-							product={product}
-						/>
-					)),
-				)}
-			</div>
-			{hasNextPage && (
-				<div ref={nextFetchTargetRef} className="col-span-2"></div>
-			)}
-			<Button
-				className="fixed bottom-4 right-4 px-3 py-6 rounded-full shadow-lg"
-				onClick={scrollToTop}
-			>
-				<ArrowUp />
-			</Button>
-		</div>
-	);
+    return (
+        <div className="w-full mt-4 flex flex-col content-center justify-center align-middle items-center overflow-y-auto scrollbar-hide">
+            <h2 className="text-xl font-bold"> Curation </h2>
+            <div className="container py-5 px-2 grid grid-cols-2 gap-2">
+                {data?.pages.map((page, pageIndex) =>
+                    page.map((product: Product, productIndex: number) => (
+                        <ProductCard
+                            key={`${pageIndex}-${productIndex}`}
+                            product={product}
+                        />
+                    )),
+                )}
+            </div>
+            {hasNextPage && (
+                <div ref={nextFetchTargetRef} className="col-span-2"></div>
+            )}
+            <Button
+                className="fixed bottom-16 right-0 px-3 py-6 rounded-full shadow-lg m-1"
+                onClick={scrollToTop}
+            >
+                <ArrowUp />
+            </Button>
+        </div>
+    );
 };
 
 export default Curation;
