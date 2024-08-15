656282c3e277b586140addc1085092e1086ceeb5
R3D1NM <r3dinm@gmail.com>
2024-08-15 22:26:48 +0900

    [feat] discover page under construction (ONE-516)

---
+++ src/pages/Discover/Discover.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Discover = () => {
+    return (
+        <div className="w-full pb-5 pt-4">
+            <div className="flex flex-col">
+                <h3 className="text-3xl">Discover Page</h3>
+                <h5 className="text-xl">UNDER CONSTRUCTION</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Discover;
