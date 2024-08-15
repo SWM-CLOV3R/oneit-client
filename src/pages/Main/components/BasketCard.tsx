fdd6204edf8cc4f53f8f366a59cc7a7a82e06930
R3D1NM <r3dinm@gmail.com>
2024-08-15 11:22:06 +0900

    [fix] basket card login redirection path (ONE-537)

--- src/pages/Main/components/BasketCard.tsx
+++ src/pages/Main/components/BasketCard.tsx
@@ -14,7 +14,7 @@ const BasketCard = ({text, login}: {text: string; login: boolean}) => {
         <>
             <Card className="w-full bg-gradient-to-br from-oneit-blue to-[#a3dbff] hover:from-[#a3dbff] hover:to-[#98d5fb] transition-colors duration-300 rounded-lg h-[50%] flex flex-col justify-between shadow-md border-0 max-w-md">
                 <Link
-                    to={login ? '/basket/create' : '/login'}
+                    to={login ? '/basket/create' : '/login?redirect=/'}
                     className="w-full h-full flex flex-col justify-between"
                 >
                     <CardHeader className="p-3">
