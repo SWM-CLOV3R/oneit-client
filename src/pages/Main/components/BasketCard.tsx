17178eafd1495b814363f856382fe5f304f17596
R3D1NM <r3dinm@gmail.com>
2024-09-12 14:35:38 +0900

    [style] add animation on reccomend card (ONE-633)

--- src/pages/Main/components/BasketCard.tsx
+++ src/pages/Main/components/BasketCard.tsx
@@ -1,30 +1,21 @@
 import {Link, useNavigate} from 'react-router-dom';
-
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
 import {Button} from '@/components/ui/button';
 
 const BasketCard = ({text, login}: {text: string; login: boolean}) => {
     return (
         <>
-            <div className="w-full bg-gradient-to-br from-oneit-blue to-[#a3dbff] hover:from-[#a3dbff] hover:to-[#98d5fb] transition-colors duration-300 rounded-lg h-[20%] flex flex-col justify-between shadow-md border-0 max-w-md">
+            <div className="w-full bg-gradient-to-br from-oneit-blue to-[#a3dbff] hover:from-[#a3dbff] hover:to-[#98d5fb] transition-colors duration-300 rounded-lg h-fit flex flex-col justify-between shadow-md border-0 max-w-md">
                 <Link
                     to={login ? '/basket' : '/login?redirect=/basket'}
                     className="w-full h-full flex flex-col justify-between"
                 >
-                    <div className="p-3">
+                    <div className="mx-3 mt-2">
                         <h3 className="text-2xl font-bold">이 중에 뭐 주지?</h3>
                         <p className="text-oneit-gray text-sm">
                             뭘 좋아할지 몰라서 다 준비해봤어
                         </p>
                     </div>
-                    <Button className="m-2 bg-white hover:bg-slate-50">
+                    <Button className="relative m-2 bg-white hover:bg-slate-50 text-md">
                         {text}
                     </Button>
                 </Link>
--- src/pages/Main/components/RecommendCard.tsx
+++ src/pages/Main/components/RecommendCard.tsx
@@ -5,23 +5,28 @@ import {
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
+import LottieContainer from '@/components/common/LottieContainer';
 
 const RecommendCard = () => {
     return (
         <>
-            <Card className="w-full bg-gradient-to-br from-[#ffa0a0] to-[#ff9595] hover:from-[#FF9595] hover:to-[#FF9090] transition-colors duration-300 rounded-lg h-[50%] flex flex-col justify-between shadow-md border-0 max-w-md">
+            <div className="p-1 w-full bg-gradient-to-br from-[#ffa0a0] to-[#ff9595] hover:from-[#FF9595] hover:to-[#FF9090] transition-colors duration-300 rounded-lg h-[50%] flex flex-col justify-between shadow-md border-0 max-w-md">
                 <Link
                     to="recommend"
                     className="w-full h-full flex flex-col justify-between"
                 >
-                    <CardHeader className="p-3">
-                        <CardTitle>선물 뭐 주지?</CardTitle>
-                        <CardDescription className="text-oneit-gray">
+                    <div className="px-3 pt-3">
+                        <h3 className="text-2xl font-bold">선물 뭐 주지?</h3>
+                        <p className="text-oneit-gray text-sm">
                             어떤 선물을 줘야 좋아할까? 선물 취향 테스트
-                        </CardDescription>
-                    </CardHeader>
+                        </p>
+                    </div>
+                    <LottieContainer
+                        fileName="surpriseGift_animate.json"
+                        className="relative w-full h-[80%]"
+                    />
                 </Link>
-            </Card>
+            </div>
         </>
     );
 };
