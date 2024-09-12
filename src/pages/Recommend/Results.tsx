e84a6fe5a902ba230498ed2c47c37bc5423cf3de
R3D1NM <r3dinm@gmail.com>
2024-09-12 17:31:56 +0900

    [feat] event popup on rec. result (ONE-633)

--- src/pages/Recommend/Results.tsx
+++ src/pages/Recommend/Results.tsx
@@ -14,6 +14,8 @@ import React, {Suspense, useEffect, useState} from 'react';
 import {
     Dialog,
     DialogContent,
+    DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
@@ -32,12 +34,12 @@ import Share from '@/components/common/Share';
 import KakaoShare from '@/components/common/KakaoShare';
 import {isLoginAtom} from '@/api/auth';
 import {toast} from 'sonner';
-import {Link} from 'lucide-react';
+import {GiftIcon, Link} from 'lucide-react';
 const GiftCard = React.lazy(() => import('./components/GiftCard'));
 const NotFound = React.lazy(() => import('../NotFound'));
 
 const Results = () => {
-    const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(true);
     const navigate = useNavigate();
     const {chatID} = useParams();
     const isLogin = useAtomValue(isLoginAtom);
@@ -202,6 +204,89 @@ const Results = () => {
                                     </Button>
                                 </div>
                             )}
+                            <div className="flex items-center">
+                                <Dialog defaultOpen={true}>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            variant="ghost"
+                                            className="fixed bottom-16 right-0 px-3 py-6 rounded-full shadow-lg m-1 bg-gradient-to-br from-oneit-blue to-oneit-pink text-oneit-gray hover:from-oneit-pink hover:to-oneit-blue"
+                                        >
+                                            <GiftIcon className="w-6 h-6" />
+                                        </Button>
+                                    </DialogTrigger>
+                                    <DialogContent>
+                                        <DialogHeader>
+                                            <DialogTitle>
+                                                ONE!T 선물 추천 이벤트 진행 중
+                                            </DialogTitle>
+                                            <DialogDescription className="text-oneit-gray">
+                                                <p>
+                                                    선물 추천 결과를 공유하고
+                                                    경품을 받아가세요!
+                                                </p>
+                                            </DialogDescription>
+                                        </DialogHeader>
+                                        <div className="flex flex-col gap-2 text-center">
+                                            <div>
+                                                <strong className="">
+                                                    설문조사 이벤트
+                                                </strong>
+                                                <p className="text-start">
+                                                    추천 결과 페이지 링크를
+                                                    복사하여 <br />
+                                                    아래 폼에 등록해주시면{' '}
+                                                    <br />
+                                                    추첨을 통해 총 3 분께 <br />
+                                                    추천 상품 중 하나를
+                                                    드립니다.
+                                                </p>
+                                            </div>
+                                            <div className="flex justify-around gap-2 w-full">
+                                                <Button
+                                                    onClick={async () => {
+                                                        await navigator.clipboard.writeText(
+                                                            `https://oneit.gift/result/${chatID}`,
+                                                        );
+                                                        toast(
+                                                            '클립보드에 복사되었습니다.',
+                                                        );
+                                                    }}
+                                                    className="w-full"
+                                                >
+                                                    결과 링크 복사
+                                                </Button>
+                                                {/* todo: instagram event link */}
+                                                <a className="w-full">
+                                                    <Button className="w-full">
+                                                        SNS 공유하기
+                                                    </Button>
+                                                </a>
+                                            </div>
+                                            <div>
+                                                <strong>
+                                                    인스타그램 이벤트
+                                                </strong>
+                                                <p className="text-start">
+                                                    추천 결과 페이지를 캡쳐하여{' '}
+                                                    <br />
+                                                    인스타그램에 공유하면 <br />
+                                                    추첨을 통해 총 10 분께{' '}
+                                                    <br />
+                                                    스타벅스 기프티콘을
+                                                    드립니다.
+                                                </p>
+                                            </div>
+                                        </div>
+
+                                        <a>
+                                            <Button className="w-full">
+                                                인스타그램 이벤트 페이지
+                                                바로가기
+                                            </Button>
+                                        </a>
+                                    </DialogContent>
+                                </Dialog>
+                            </div>
                         </div>
                     )}
                 </Card>
