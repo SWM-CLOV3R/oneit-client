b39f46a31653e54af41431299f5e7576f05aee61
R3D1NM <r3dinm@gmail.com>
2024-09-16 22:15:35 +0900

    [feat] after inquiry if login (ONE-660)

--- src/pages/Inquiry/AfterInquiry.tsx
+++ src/pages/Inquiry/AfterInquiry.tsx
@@ -1,9 +1,12 @@
+import {isLoginAtom} from '@/api/auth';
 import {Button} from '@/components/ui/button';
+import {useAtomValue} from 'jotai';
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 
 const AfterInquiry = () => {
     const navigate = useNavigate();
+    const isLogin = useAtomValue(isLoginAtom);
     return (
         <div className="flex flex-col content-center w-full gap-2 justify-center items-center">
             <h3 className="text-xl">감사합니다!</h3>
@@ -16,12 +19,14 @@ const AfterInquiry = () => {
             />
             <div className="flex flex-col gap-2 w-full">
                 <Button onClick={() => navigate('/')}>메인으로</Button>
-                <Button
-                    onClick={() => navigate('/login')}
-                    className="bg-kakao-yellow hover:bg-kakao-yellow/90"
-                >
-                    로그인하고 나를 위한 선물 고르기
-                </Button>
+                {!isLogin && (
+                    <Button
+                        onClick={() => navigate('/login')}
+                        className="bg-kakao-yellow hover:bg-kakao-yellow/90"
+                    >
+                        로그인하고 나를 위한 선물 고르기
+                    </Button>
+                )}
             </div>
         </div>
     );
