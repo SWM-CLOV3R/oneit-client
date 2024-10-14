6a39e58bdd4b086c1adfb41a295e64a3e5f60a5e
R3D1NM <r3dinm@gmail.com>
2024-10-14 16:37:53 +0900

    [fix] unavailable images on header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,6 +2,7 @@ import {useAtomValue} from 'jotai';
 import {isLoginAtom} from '@/api/auth';
 import logoImage from '@/assets/logo.svg';
 import mypageIcon from '@/assets/icon_mypage.svg';
+
 const Header = () => {
     const isLogin = useAtomValue(isLoginAtom);
 
@@ -12,21 +13,25 @@ const Header = () => {
             window.location.href = '/login';
         }
     };
+
     return (
         <header className="fixed top-0 left-0 right-0 bg-white flex h-14 items-center justify-between px-4 md:px-6 shadow-md z-50">
             <div className="flex items-center">
                 <a href="/main" className="flex items-center">
-                    <div
-                        className="w-[4.75rem] h-[2.125rem] bg-no-repeat bg-center bg-contain"
-                        style={{backgroundImage: `url(${logoImage})`}}
+                    <img
+                        src={logoImage}
+                        alt="Logo"
+                        className="w-[4.75rem] h-[2.125rem] object-contain"
                     />
                 </a>
             </div>
-            <button
-                onClick={toMypage}
-                className="w-9 h-9 bg-no-repeat bg-center bg-contain"
-                style={{backgroundImage: `url(${mypageIcon})`}}
-            />
+            <button onClick={toMypage} className="w-9 h-9">
+                <img
+                    src={mypageIcon}
+                    alt="My Page"
+                    className="w-full h-full object-contain"
+                />
+            </button>
         </header>
     );
 };
