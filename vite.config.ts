cb0dd71a82b91fe817e24125c7ad0a91cd19c271
R3D1NM <r3dinm@gmail.com>
2024-10-26 10:44:00 +0900

    [fix] sentry error invoke

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
+import {sentryVitePlugin} from '@sentry/vite-plugin';
 
 // Assuming your environment variable is named VITE_API_URL
 // and you have a .env file in your project root with VITE_API_URL defined
@@ -27,6 +28,10 @@ export default ({mode}: {mode: string}) => {
                     __dirname,
                     './cert/localhost-key.pem',
                 ),
+            }),
+            sentryVitePlugin({
+                org: 'clov3r',
+                project: 'oneit-front',
             }),
         ],
         resolve: {
